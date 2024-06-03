054b052abeedb2b7d0830efafc19ed305e5e41d1
Ankush H V <anshhv2003@gmail.com>
2024-06-04 01:18:44 +0530

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // experimental: {
-    //     esmExternals: "loose", // <-- add this
-    //     serverComponentsExternalPackages: ["mongoose"] // <-- and this
-    // },
+    experimental: {
+        // esmExternals: "loose", // <-- add this
+        serverComponentsExternalPackages: ["mongoose"] // <-- and this
+    },
     // webpack: (config) => {
     //     config.experiments = {
     //         topLevelAwait: true
